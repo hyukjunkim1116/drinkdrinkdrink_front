89e04cc75e6e6c7e5e337ac40cbccfb505ffe2e9
Hyuk Jun Kim <nmkk1234@naver.com>
2023-05-14 17:40:59 +0900

    fix imageBox

--- js/index.js
+++ js/index.js
@@ -13,10 +13,10 @@ window.onload = async function () {
     imageBox.setAttribute("class", "img-box");
     imageBox.setAttribute("onclick", `moveDetail(${article.id})`);
 
-    if (article.image) {
-      imageBox.setAttribute("src", `${backend_base_url}${article.image}`);
-    } else {
+    if (!article.image || article.image == null) {
       imageBox.setAttribute("src", `${no_image}`);
+    } else {
+      imageBox.setAttribute("src", `${backend_base_url}${article.image}`);
     }
     contentBox.append(imageBox);
   });
